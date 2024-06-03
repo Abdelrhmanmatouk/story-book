11c6b96ac7bfdd4d3dac0e474b910ed5b653c7e2
Abdelrhmanmatouk <abdomatouk97@gmail.com>
2024-06-03 21:54:16 +0300

    adding sessions & saving it to database

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-const path = require('path')
+const path = require("path");
 const express = require("express");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
-const passport = require('passport');
-const session = require('express-session')
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")
 const connectDB = require("./config/db");
 
 // load config
 dotenv.config({ path: "./config.env" });
 
 // passport config
-require('./config/passport')(passport)
+require("./config/passport")(passport);
 
 connectDB();
 
@@ -23,31 +25,32 @@ if (process.env.NODE_ENV === "development") {
 
 //handlebars
 
-app.engine('.hbs', exphbs.engine({defaultLayout:'main', extname: '.hbs'}));
-app.set('view engine', '.hbs');
+app.engine(".hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", ".hbs");
 
 // sessions
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: false,
-
-}))
-
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongoUrl: process.env.MONGO_URI}),
+  })
+);
 
 // passport middleware
 
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
-// static 
+// static
 
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 
-app.use('/',require('./routes/index'))
-app.use('/auth',require('./routes/auth'))
+app.use("/", require("./routes/index"));
+app.use("/auth", require("./routes/auth"));
 
 const port = process.env.PORT || 5000;
 app.listen(
